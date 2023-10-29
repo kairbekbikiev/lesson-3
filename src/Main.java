53036f465748a7ca9992f5760cc1dbdcc76d44d4
Kairbekbikiev <kairbekbikiev@gmail.com>
2023-10-29 23:52:06 +0600

    Add lesson 3

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("\t\t\tДобро пожаловать в валютный калькулятор");
+//        System.out.println("В " +10+ " долларах :" + (10*97)+" Рублей");
+//        System.out.println("В " +5+ " долларах:" + (5*97)+" Рублей");
+//        System.out.println("В " +6+ " долларах:" + (6*97)+" Рублей");
+
+// масса (кг)/ рост2 (м2)
+        System.out.println("\t\t\t****Добро пожаловать в калькулятор BMI****");
+        System.out.println("BMI:" + 82.0 / (175 * 175));
+    }
+}
